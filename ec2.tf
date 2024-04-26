1ae930f0f86ebd5adca84b436c00f3475c6deaf7
Daniel-yepez <59668714+Daniel-yepez@users.noreply.github.com>
2024-04-26 13:27:55 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -19,7 +19,7 @@ resource "aws_instance" "aws-deployment-ec2" {
   ami             = "ami-0be2609ba883822ec"
   instance_type   = "t2.micro"
   security_groups = ["default"]
-  subnet_id       = aws_subnet.aws-deployment-subnet.id
+  subnet_id       = "subnet-0d6a1e5902d68ce9e"
 
   tags = {
     Name = "demo-ec2"
