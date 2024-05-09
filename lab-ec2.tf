e998605067b3178d9d48dc72ebbae1ec3a3982c7
Daniel-yepez <59668714+Daniel-yepez@users.noreply.github.com>
2024-05-09 16:57:26 -0500

    Create lab-ec2.tf

---
+++ lab-ec2.tf
@@ -0,0 +1,26 @@
+resource "aws_vpc" "aws-deployment-vpc-lab" {
+  cidr_block = "172.17.0.0/16"
+
+  tags = {
+    Name = "demo-vpc-lab"
+  }
+}
+
+resource "aws_subnet" "aws-deployment-subnet-lab" {
+  vpc_id     = aws_vpc.aws-deployment-vpc-lab.id
+  cidr_block = "172.17.20.0/24"
+
+  tags = {
+    name = "demo-subnet-lab"
+  }
+}
+
+resource "aws_instance" "aws-deployment-ec2-lab" {
+  ami             = "ami-0be2609ba883822ec"
+  subnet_id       = aws_subnet.aws-deployment-subnet-lab.id
+  instance_type   = "t2.micro"
+
+  tags = {
+    Name = "lab-new-ec2"
+  }
+}
